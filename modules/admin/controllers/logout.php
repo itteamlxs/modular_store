af3831e7c787ba25375b1e51e3df539335527f96
Neo Coder <leon.jass@outlook.com>
2025-07-24 15:38:37 -0600

    reparando login de admins

--- modules/admin/controllers/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/../../../core/bootstrap.php';
-
-session_destroy();
-header('Location: /modular-store/modules/admin/controllers/login.php');
-exit;
